4fa54db5cfa441e18e50a0b90b18218429b5e434
hans-grimlund <hans@grimlunds.com>
2024-01-12 13:59:38 +0100

    Create ConnectionString.cs

---
+++ Auktionssajt.Data/ConnectionString.cs
@@ -0,0 +1,7 @@
+namespace Auktionssajt.Data
+{
+    public static class ConnectionString
+    {
+        public static readonly string str = "Server=tcp:jensen-sql-server.database.windows.net,1433;Initial Catalog=Auktionssajt;Persist Security Info=False;User ID=Auktionssajt;Password=Jensen123);MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+    }
+}
