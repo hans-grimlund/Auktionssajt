ce7fd4f67f269408e8908a58100b3e839bfe2151
hans-grimlund <hans@grimlunds.com>
2024-01-16 17:04:36 +0100

    Wrote MappingService

--- Auktionssajt.Core/Interfaces/IMappingService.cs
+++ Auktionssajt.Core/Interfaces/IMappingService.cs
@@ -1,7 +1,16 @@
+using Auktionssajt.Domain.DTOs;
+using Auktionssajt.Domain.Entities;
+using Auktionssajt.Domain.Models;
+
 namespace Auktionssajt.Core.Interfaces
 {
     public interface IMappingService
     {
-        
+        UserEntity NewUserModelToUserEntity(NewUserModel model);
+        UserDTO UserEntityToUserDTO(UserEntity entity);
+        List<UserDTO> UserEntityToUserDTO(List<UserEntity> entities);
+        AuctionEntity NewAuctionModelToAuctionEntity(NewAuctionModel model);
+        AuctionDTO AuctionEntityToAuctionDTO(AuctionEntity entity);
+        List<AuctionDTO> AuctionEntityToAuctionDTO(List<AuctionEntity> entities);
     }
 }
--- Auktionssajt.Core/Services/MappingService.cs
+++ Auktionssajt.Core/Services/MappingService.cs
@@ -1,7 +1,62 @@
+using Auktionssajt.Core.Interfaces;
+using Auktionssajt.Domain.DTOs;
+using Auktionssajt.Domain.Entities;
+using Auktionssajt.Domain.Models;
+
 namespace Auktionssajt.Core.Services
 {
-    public class MappingService
+    public class MappingService : IMappingService
     {
-        
+        public AuctionDTO AuctionEntityToAuctionDTO(AuctionEntity entity)
+        {
+            return new AuctionDTO()
+            {
+                // TODO Code this
+            };
+        }
+
+        public List<AuctionDTO> AuctionEntityToAuctionDTO(List<AuctionEntity> entities)
+        {
+            List<AuctionDTO> DTOs = [];
+            foreach (var entity in entities)
+            {
+                DTOs.Add(AuctionEntityToAuctionDTO(entity));
+            }
+            return DTOs;
+        }
+
+        public AuctionEntity NewAuctionModelToAuctionEntity(NewAuctionModel model)
+        {
+            return new AuctionEntity()
+            {
+                // TODO Code this
+            };
+        }
+
+        public UserEntity NewUserModelToUserEntity(NewUserModel model)
+        {
+            return new UserEntity()
+            {
+                // TODO Code this
+            };
+        }
+
+        public UserDTO UserEntityToUserDTO(UserEntity entity)
+        {
+            return new UserDTO()
+            {
+                // TODO Code this
+            };
+        }
+
+        public List<UserDTO> UserEntityToUserDTO(List<UserEntity> entities)
+        {
+            List<UserDTO> DTOs = [];
+            foreach (var entity in entities)
+            {
+                DTOs.Add(UserEntityToUserDTO(entity));
+            }
+            return DTOs;
+        }
     }
 }
