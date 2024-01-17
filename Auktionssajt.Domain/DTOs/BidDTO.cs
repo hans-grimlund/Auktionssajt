589e8dd36ec6e15ae120d5523928980b90d5382b
PontusLindblom91 <146932541+PontusLindblom91@users.noreply.github.com>
2024-01-17 10:37:13 +0100

    Update
    BidRepo skapad, bidentity skapad

--- Auktionssajt.Domain/DTOs/BidDTO.cs
+++ Auktionssajt.Domain/DTOs/BidDTO.cs
@@ -2,6 +2,11 @@ namespace Auktionssajt.Domain.DTOs
 {
     public class BidDTO
     {
-        
+
+        public int BidId { get; set; }
+        public decimal BidPrice { get; set; }
+        public DateTime BidDateTime { get; set; }
+        public int AuctionId { get; set; }
+
     }
 }
