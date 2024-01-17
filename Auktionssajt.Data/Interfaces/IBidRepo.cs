25ce078716894c56e54611efd9542b9f2d7801c2
PontusLindblom91 <146932541+PontusLindblom91@users.noreply.github.com>
2024-01-17 14:13:59 +0100

    Updated stuff

--- Auktionssajt.Data/Interfaces/IBidRepo.cs
+++ Auktionssajt.Data/Interfaces/IBidRepo.cs
@@ -1,7 +1,11 @@
+using Auktionssajt.Domain.Entities;
+
 namespace Auktionssajt.Data.Interfaces
 {
     public interface IBidRepo
     {
-        
+        void InsertBid(BidEntity bid);
+        void DeleteBid(int id);
+        List<BidEntity> GetBids(int id);
     }
 }
